d393b4ee4e02970c0ac3ea18791c305c3133ff8d
isdavimoraes <123647859+isdavimoraes@users.noreply.github.com>
2023-03-01 11:46:08 -0300

    Update Main.py
    Criei uma função Pesquisa contendo um  while que salva os dados do entrevistado no dicionário dados.

--- Projeto em Grupo Módulo 2/Main.py
+++ Projeto em Grupo Módulo 2/Main.py
@@ -7,26 +7,26 @@ from Censo import *
 class main():
     pass
 
-lista=[]
-idade=int(input('idade:'))
-genero=input('gênero:')
-q1=input('reforma previdenciária? (s/n/nsr) \n')
-q2=input('reforma trabalhista? (s/n/nsr) \n')
-q3=input('reforma tributária? (s/n/nsr) \n')
-q4=input('reforma do código penal? (s/n/nsr) \n')
-info=Entrevistado(idade,genero)
-quest=Questoes(q1,q2,q3,q4)
-lista.append(info.idade)
-lista.append(info.genero)
-lista.append(quest.q1)
-lista.append(quest.q2)
-lista.append(quest.q3)
-lista.append(quest.q4)
-print(lista)
+def Pesquisa():
+    idade = ''
+    dados= {}
+    print('Seja bem vindo(a) a nossa pesquisa!\nCaso deseje parar, favor digitar 00 na idade.')
+    while idade != '00':
+                
+        idade=input('idade:')
+        if idade == '00':
+            print('Dados coletados.\nObrigado por fazer parte da nossa pesquisa')
+            break
+        genero=input('gênero:')
+        q1=input('reforma previdenciária? (s/n/nsr) \n')
+        q2=input('reforma trabalhista? (s/n/nsr) \n')
+        q3=input('reforma tributária? (s/n/nsr) \n')
+        q4=input('reforma do código penal? (s/n/nsr) \n')
+        quest=Questoes(idade,genero,q1,q2,q3,q4)
+        dados[quest.idade, quest.genero]=(quest.q1,quest.q2,quest.q3,quest.q4)
+        print('Você deseja continuar? Caso não, favor digitar 00 na idade.')
+        print(dados)
 
 
-
-'''teste=Final()
-print(teste.__init__)'''
-
+Pesquisa()
 
