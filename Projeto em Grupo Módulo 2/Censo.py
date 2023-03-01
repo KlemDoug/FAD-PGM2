23432cc2c8257eeb0dfef37cbc084b9a054abb09
isdavimoraes <123647859+isdavimoraes@users.noreply.github.com>
2023-03-01 11:42:24 -0300

    Update Censo.py
    Correção da classe Questoes.
    Atualização para a classe Questoes herdar os atributos da classe Entrevistado.

--- Projeto em Grupo Módulo 2/Censo.py
+++ Projeto em Grupo Módulo 2/Censo.py
@@ -1,34 +1,14 @@
-# etapa 2 - classes:
+# etapa 2 - classes (Censo.py):
 
 class Entrevistado:
     def __init__(self,idade,genero):
         self.idade=idade
         self.genero=genero
 
-class Questoes:
-    def __init__(self,q1,q2,q3,q4):
+class Questoes(Entrevistado):
+    def __init__(self,idade,genero,q1,q2,q3,q4):
+        super().__init__(idade, genero)
         self.q1=q1
         self.q2=q2
         self.q3=q3
         self.q4=q4
-'''
-class Final:
-    def __init__(self):
-        lista=[]
-        info=Entrevistado()
-        quest=Questoes()
-        while idade!=00:
-            idade=int(input('idade:'))
-            genero=input('gênero:')
-            q1=input('reforma previdenciária? (s/n/nsr) \n')
-            q2=input('reforma trabalhista? (s/n/nsr) \n')
-            q3=input('reforma tributária? (s/n/nsr) \n')
-            q4=input('reforma do código penal? (s/n/nsr) \n')
-            for i in self.lista:
-                lista.append(info)
-                lista.append(quest)
-            print(lista) 
-        else:
-            print('dados coletados.')
-Final()
-'''
