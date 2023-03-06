e1911056d64cceba224b04803817f5713f338507
isdavimoraes <123647859+isdavimoraes@users.noreply.github.com>
2023-03-05 21:52:44 -0300

    Update Censo.py
    Foi adicionado o parâmetro dataehora na Class Entrevistado.

--- Projeto em Grupo Módulo 2/Censo.py
+++ Projeto em Grupo Módulo 2/Censo.py
@@ -6,9 +6,10 @@ class Entrevistado:
         self.genero=genero
 
 class Questoes(Entrevistado):
-    def __init__(self,idade,genero,q1,q2,q3,q4):
+    def __init__(self,idade,genero,q1,q2,q3,q4,dataehora):
         super().__init__(idade, genero)
         self.q1=q1
         self.q2=q2
         self.q3=q3
         self.q4=q4
+        self.dataehora= dataehora
