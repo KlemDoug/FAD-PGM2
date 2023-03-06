6f244b9a4a7403e3eeabf831fd65478eac560bb9
isdavimoraes <123647859+isdavimoraes@users.noreply.github.com>
2023-03-05 21:51:11 -0300

    Update Main.py
    Foi realizado alterações na forma de salvar os dados, antes era salvo em um dicionário e agora está sendo salvo em uma lista para melhor funcionamento.
    Foi adicionado tratamento de dados em todas as perguntas para que evite respostas inválidas pelo usuário.
    As perguntas foram reformuladas para um melhor entendimento.
    Foi adicionado data e hora quando toda vez que o usuário responder o questionário com respostas válidas.
    Foram adicionados alguns comentários para melhor entendimento do código.

--- Projeto em Grupo Módulo 2/Main.py
+++ Projeto em Grupo Módulo 2/Main.py
@@ -1,6 +1,6 @@
 import csv
 from time import *
-from datetime import date
+import datetime
 from Censo import *
 
 
@@ -9,37 +9,66 @@ class main():
 
 
 def Pesquisa():
+                  ################ Variáveis ################
+#######################################################################################
     idade = ''
-    dados = {}
+    dados = []
+    arquivar = open("csvteste.csv", "w", newline="", encoding="utf-8")
     print('Seja bem vindo(a) a nossa pesquisa!\nCaso deseje parar, favor digitar 00 na idade.')
+    gravador = csv.writer(arquivar)
+    cabecalho=['Idade','Genero','Questão 1','Questão 2','Questão 3','Questão 4','Data e Hora']
+    gravador.writerow(cabecalho)    
+    data_e_hora_atuais = datetime.datetime.now()
+    data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y , %H:%M')
+#######################################################################################
     while idade != '00':
-
-        idade = input('idade:')
+        respostas_permitidas = 'S'and'N'and'NSR'
+        idade = input('Quantos anos você tem? \n')
+        idade_limpeza = idade.isdigit() #Variável que retorn true se idade é válida.
+        if idade_limpeza is False: #Se o usuario digitar uma idade invalida irá aparecer essa mensagem.
+            print('Você não digitou uma idade válida!')
+            continue
         if idade == '00':
             print('Dados coletados.\nObrigado por fazer parte da nossa pesquisa')
             break
-        genero = input('gênero:')
-        q1 = input('reforma previdenciária? (s/n/nsr) \n')
-        q2 = input('reforma trabalhista? (s/n/nsr) \n')
-        q3 = input('reforma tributária? (s/n/nsr) \n')
-        q4 = input('reforma do código penal? (s/n/nsr) \n')
-        quest = Questoes(idade, genero, q1, q2, q3, q4)
-        dados[quest.idade, quest.genero] = (
-            quest.q1, quest.q2, quest.q3, quest.q4)
+        genero = input('Qual gênero você se identifica?\n').upper()
+        #Primeira pergunta e tratamendo de dados.
+        q1 = input('Você é a favor da reforma previdenciária? (s/n/nsr) \n').upper()
+        if q1 not in respostas_permitidas:
+            print('Resposta Inválida.')
+            continue
+        #Segunda pergunta e tratamendo de dados.
+        q2 = input('Você é a favor da reforma trabalhista? (s/n/nsr) \n').upper()
+        if q2 not in respostas_permitidas:
+            print('Resposta Inválida.')
+            continue 
+        #Terceira pergunta e tratamendo de dados.
+        q3 = input('Você é a favor da reforma tributária? (s/n/nsr) \n').upper()
+        if q3 not in respostas_permitidas:
+            print('Resposta Inválida.')
+            continue 
+        #Quarta pergunta e tratamendo de dados.               
+        q4 = input('Você é a favor da reforma do código penal? (s/n/nsr) \n').upper()
+        if q4 not in respostas_permitidas:
+            print('Resposta Inválida.')
+            continue
+            
+        dataehora = data_e_hora_em_texto
+        #Declarando os paramentros da Classe Questões.
+        quest = Questoes(idade, genero, q1, q2, q3, q4, dataehora)
+        #Lista onde os dados estão sendo guardados.
+        dados = [quest.idade, quest.genero,
+            quest.q1, quest.q2, quest.q3, quest.q4, dataehora]
         print('Você deseja continuar? Caso não, favor digitar 00 na idade.')
         print(dados)
+        #Criando o arquivo CSV onde será guardado os dados.
+        arquivar = open("csvteste.csv", "a", newline="", encoding="utf-8") #arquivar = open("Projeto em Grupo Módulo 2/csvteste.csv", "w", newline="", encoding="utf-8")
 
-        arquivar = open("Projeto em Grupo Módulo 2/csvteste.csv", "w", newline="", encoding="utf-8")
+        #Gravando dados no CSV.
         gravador = csv.writer(arquivar)
-        cabecalho=['cabecalho de teste']
-        gravador.writerow(cabecalho)
-        dadosk=dados.keys()
-        dadosv=dados.values()
-        for i,j in zip(dadosk,dadosv):
-            gravador.writerow(i)
-            gravador.writerow(j)
+        gravador.writerow(dados)
+        #Fechando Arquivo.
         arquivar.close()
 
 
 Pesquisa()
-
