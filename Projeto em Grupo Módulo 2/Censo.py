bebc5c613bac279cf14b0111b18d27b1c57d85b9
Douglas Klem <120439075+KlemDoug@users.noreply.github.com>
2023-03-07 01:05:59 -0300

    att 07/03/23
    adição de comentários ao arquivo.

--- Projeto em Grupo Módulo 2/Censo.py
+++ Projeto em Grupo Módulo 2/Censo.py
@@ -1,15 +1,18 @@
-# etapa 2 - classes (Censo.py):
-
+# Arquivo de apoio ao Main.py com as classes que são importadas.
+# Classe herdada Entrevistado.
 class Entrevistado:
-    def __init__(self,idade,genero):
-        self.idade=idade
-        self.genero=genero
+    def __init__(self, idade, genero):
+        self.idade = idade
+        self.genero = genero
 
+
+# Classe herdeira Questoes.
 class Questoes(Entrevistado):
-    def __init__(self,idade,genero,q1,q2,q3,q4,dataehora):
+    def __init__(self, idade, genero, q1, q2, q3, q4, dataehora):
         super().__init__(idade, genero)
-        self.q1=q1
-        self.q2=q2
-        self.q3=q3
-        self.q4=q4
-        self.dataehora= dataehora
+        self.q1 = q1
+        self.q2 = q2
+        self.q3 = q3
+        self.q4 = q4
+        self.dataehora = dataehora
+
